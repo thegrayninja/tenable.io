25b45f31d68188633f20b6d7b59b25e6e4902424
thegrayninja <grayninja@protonmail.com>
2017-03-24 11:35:42 -0700

    Update agent_list.py

--- agent_list.py
+++ agent_list.py
@@ -1,6 +1,6 @@
 #github.com/thegrayninja
 #for use with: curl -H "X-ApiKeys: accessKey=; secretKey=" https://cloud.tenable.com/scanners/1/agents >> all_agents.json 
-#currently, simply returns all agent IP, Hostname and OS
+#currently, simply returns all agent IP, Hostname, OS and ID
 
 import json
 originalFile = raw_input("Please Enter JSON file name: ")
@@ -12,8 +12,9 @@ with open (originalFile, "r") as data_file:
 		agent_ip = (data["agents"][counter]["ip"])
 		agent_os = (data["agents"][counter]["platform"])
 		agent_name = (data["agents"][counter]["name"])
+		agent_id = (data["agents"][counter]["id"])
 		counter += 1
-		results = results + "\n%s,%s,%s" % (agent_ip, agent_name, agent_os)
+		results = results + "\n%s,%s,%s,%s" % (agent_ip, agent_name, agent_os, agent_id)
 
 
 newFile = open(originalFile.replace('.json', '.csv'), "w")
