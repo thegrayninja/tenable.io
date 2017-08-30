78ea87755e80605e45ed1e8ba250c27418331272
thegrayninja <grayninja@protonmail.com>
2017-08-30 15:13:15 -0700

    Update asset_vulns.py

--- asset_vulns.py
+++ asset_vulns.py
@@ -1,7 +1,7 @@
 #github.com/thegrayninja
-#2.1.1
+#2.1.2
 
-#v2.1.1 - cleaning up code
+#v2.1.2 - cleaning up code, removing temporary files. Also updated link so it returns correct data!!
 #changes were made on tenable's back end, having to revamp the old version..and this is the result
 #v2.0 involves single lookup. v2.1 will deal with importing a file
 
@@ -31,51 +31,48 @@ def GetFileName():
 
 
 
-def VulnsToCSV(hostname):
+def VulnsToCSV(AllAssetHostnames, AssetResultsInJson):
 	results = ""
-	agent_list_File = "temp_agent_list.json"
-	with open (agent_list_File, "r") as data_file:
-		counter = 0
+	counter = 0
 
-		data = json.load(data_file)
+	data = AssetResultsInJson
 
-		for i in (data["vulnerabilities"]):
-			vuln_severity = (data["vulnerabilities"][counter]["severity"])
-			vuln_name = (data["vulnerabilities"][counter]["plugin_name"])
-			if (vuln_severity == 1):
-				vuln_severity = "low"
-			elif (vuln_severity == 2):
-				vuln_severity = "medium"
-			elif (vuln_severity == 3):
-				vuln_severity = "high"
-			else:
-				vuln_severity = "critical"
+	for i in (data["vulnerabilities"]):
+		vuln_severity = (data["vulnerabilities"][counter]["severity"])
+		vuln_name = (data["vulnerabilities"][counter]["plugin_name"])
+		if (vuln_severity == 1):
+			vuln_severity = "low"
+		elif (vuln_severity == 2):
+			vuln_severity = "medium"
+		elif (vuln_severity == 3):
+			vuln_severity = "high"
+		else:
+			vuln_severity = "critical"
 
 
-			results = results + "%s,%s,%s\n" % (hostname,vuln_severity,vuln_name)
+		results = results + "%s,%s,%s\n" % (AllAssetHostnames,vuln_severity,vuln_name)
 
-			counter += 1
+		counter += 1
 	return results
 
 
 
 
 
-def DownloadAssetVulns(AssetHostname):
+def DownloadAssetVulns(AllAssetHostnames):
 
 	#downloads as json, must save to file before parsing the contents
 	NewResults = "Hostname,Severity,Vuln_Name\n"
-	for i in AssetHostname:
+	for i in AllAssetHostnames:
 		hostname = i.lower()
 		hostname = hostname.replace("\n","")
-		agent_url = 'https://cloud.tenable.com/workbenches/vulnerabilities?authenticated=false&exploitable=false&filter.0.quality=like&filter.0.filter=host.hostname&filter.0.value=%s&resolvable=false' % (hostname)
+		agent_url = 'https://cloud.tenable.com/workbenches/vulnerabilities?filter.0.quality=match&filter.0.filter=host.hostname&filter.0.value=%s*' % (hostname)
 		agent_url_request = requests.get(agent_url, headers=tenable_header)
 
-		temp_agent_list = open("temp_agent_list.json", "w")
-		temp_agent_list.write(agent_url_request.content)
-		temp_agent_list.close()
 
-		NewResults += VulnsToCSV(hostname)
+		ResultsInJson = agent_url_request.json()
+
+		NewResults += VulnsToCSV(hostname, ResultsInJson)
 		print("....")
 		
 
