41bebeae9933139042888114a58f9960eb5b509f
thegrayninja <grayninja@protonmail.com>
2017-09-15 14:49:06 -0700

    Update get_asset_count.py
    cleaned up code to use .json() instead of saving to file, then converting to json after.

--- get_asset_count.py
+++ get_asset_count.py
@@ -1,28 +1,22 @@
 #github.com/thegrayninja
 
-import json
+
 import requests
-import time
 
 from auth_file import tenable_header
+
 print("calculating...")
 
+for i in "a":
+	TempData = requests.get('https://cloud.tenable.com/scanners/1/agents', headers=tenable_header)
+	if TempData.content == "Please retry request.":
+		print("tenable.io appears to be offline")
+	AssetData = TempData.json()
 
-tempFile_data = requests.get('https://cloud.tenable.com/scanners/1/agents', headers=tenable_header)
-if tempFile_data.content == "Please retry request.":
-	print("tenable.io appears to be offline")
-	break
-tempFile = open("temp_agent_count.json", "w")
-tempFile.write(tempFile_data.content)
-tempFile.close()
-
-
-originalFile = "temp_agent_count.json"
-with open (originalFile, "r") as data_file:
 	counter = 0
 	try:
-		data = json.load(data_file)
-		for i in (data["agents"]):
+		AssetData
+		for i in (AssetData["agents"]):
 			counter += 1
 		print ("%d agents" % (counter+1))
 	except: 
